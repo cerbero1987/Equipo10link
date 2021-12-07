a05612513a47b5e76efe7b022703ab3cdbe478f0
cerbero1987 <yestevezp@ufpso.edu.co>
2021-12-07 10:27:15 -0500

    firebase - sin probar

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,5 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:loggy/loggy.dart';
+import 'package:red_social/domain/controller/authentication_controller.dart';
+import 'package:red_social/domain/controller/chat_controller.dart';
+import 'package:red_social/domain/controller/chats_controller.dart';
+import 'package:red_social/domain/controller/firestore_controller.dart';
 
+import 'firebase_central.dart';
 import 'pages/home.dart';
 import 'pages/login.dart';
 import 'pages/register.dart';
@@ -7,7 +15,42 @@ import 'pages/chat.dart';
 import 'pages/intro.dart';
 
 class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Firebase Demo - MisionTIC',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+          body: FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            logError("error ${snapshot.error}");
+            return const Wrong();
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            Get.put(FirebaseController());
+            Get.put(AuthenticationController());
+            Get.put(ChatController());
+            Get.put(ChatsController());
+            return const FirebaseCentral();
+          }
+          return const Loading();
+        },
+      )),
+    );
+  }
+}
+
+class Wrong extends StatelessWidget {
+  const Wrong({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -35,3 +78,38 @@ class App extends StatelessWidget {
     );
   }
 }
+
+class Loading extends StatelessWidget {
+  const Loading({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const CircularProgressIndicator();
+  }
+}
+  /*@override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const IntroPage(
+              title: 'Bienvenida',
+            ),
+        '/login': (context) => const LoginPage(
+              title: 'Login',
+            ),
+        '/register': (context) => const RegisterPage(
+              title: 'Registro',
+            ),
+        '/home': (context) => const HomePage(
+              title: 'Pagina de Post',
+            ),
+        '/chat': (context) => const ChatPage(
+              title: 'Chat',
+            ),
+      },
+    );
+  }
+}*/
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:red_social/ui/app.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const App());
+    await tester.pumpWidget(App());
 /*
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
